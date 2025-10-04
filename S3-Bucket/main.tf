e5cf413cd824065a2f9ba80bb45b40eac7cd08ec
Shaik Farid <shaikfarid524@gmail.com>
2025-10-04 19:14:04 +0530

    Update main.tf

--- S3-Bucket/main.tf
+++ S3-Bucket/main.tf
@@ -1,14 +1,21 @@
+#S3 bucket creation with versioning 
+
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "key"
-  secret_key = "key"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "4oct-s3"   # must be globally unique and lowercase
-
+  bucket = "my-unique-bucket-4oct" # must be unique and lowercase
   tags = {
-    Name        = "4oct-s3"
-    Environment = "Dev"
+    Name = "MyBucket"
   }
 }
+
+resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
